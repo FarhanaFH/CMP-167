9308ab43c31daeb540f0174e1ae42f3fe6f023f9
Farhana <100227003+FarhanaFH@users.noreply.github.com>
2023-08-06 19:15:25 -0400

    Add files via upload

---
+++ CopenhagenTransit.java
@@ -0,0 +1,10 @@
+/*
+* write a program that prompts the user for their age and determine weather the user can drink alchol or not. 
+public class CopenhagenTransit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
