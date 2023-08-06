ecd3bdb92b2afe7bd080e8d7c85a1b12dabfda97
Farhana <100227003+FarhanaFH@users.noreply.github.com>
2023-08-06 19:11:20 -0400

    Add files via upload

---
+++ Program4.java
@@ -0,0 +1,9 @@
+/**
+ * Program that prompts the user for their age and determine weather the user can drink alcohol or not.
+ */
+public class Program4 {
+	public static void main(String[] args) {
+		
+	}
+
+}
